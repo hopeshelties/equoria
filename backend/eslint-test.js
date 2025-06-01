d83c27344e03264319e711533597d8f5595470fe
AthenaSB82 <athenaasb@yahoo.com>
2025-06-01 03:39:05 -0400

    Fix ES modules contamination in ESLint config

--- backend/eslint-test.js
+++
@@ -1,9 +0,0 @@
-// ESLint test file - should show errors
-const unused_variable = "this should be single quotes";
-
-function unusedFunction() {
-  console.log("double quotes error")
-  // missing semicolon
-}
-
-// This file should show multiple ESLint errors in VSCode
